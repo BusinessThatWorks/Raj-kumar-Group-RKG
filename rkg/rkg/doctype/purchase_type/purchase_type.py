6e598ddc90953bd4d023f4d427517ceb44181fed
sumandas-clapgrow <suman.das@clapgrow.com>
2026-02-17 16:53:58 +0530

    update hooks for custom script and sales all work

--- rkg/rkg/doctype/purchase_type/purchase_type.py
+++ rkg/rkg/doctype/purchase_type/purchase_type.py
@@ -4,17 +4,11 @@ from frappe.model.document import Document
 
 class PurchaseType(Document):
 
-    def validate(self):
+    def autoname(self):
 
-        # ---------------------------
-        # Mandatory Check
-        # ---------------------------
         if not self.type_name or not self.cost_center:
             frappe.throw("Type Name and Cost Center are required")
 
-        # ---------------------------
-        # Get Cost Center Abbreviation
-        # ---------------------------
         abbr = frappe.db.get_value(
             "Cost Center",
             self.cost_center,
@@ -24,12 +18,7 @@ class PurchaseType(Document):
         if not abbr:
             frappe.throw("Cost Center Abbreviation is not set")
 
-        abbr = abbr.strip()
-
-        # =====================================================
-        # 1️⃣ AUTO UPDATE DOCUMENT ID (type_name - ABBR)
-        # =====================================================
-
         clean_type = self.type_name.strip().rstrip("-").strip()
-        self.name = f"{clean_type} - {abbr}"
 
+        # Custom Name
+        self.name = f"{clean_type}-{abbr}"
